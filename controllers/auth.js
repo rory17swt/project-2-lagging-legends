354c053a22b5eeab12e56cbe0a0d59d0c62a6abc
Rory Swietlicki <rory@swietlicki.eu>
2025-04-21 11:57:27 +0100

    Added sign in and sign out pages

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import User from '../models/User.js'
+import bcrypt from 'bcryptjs'
 
 
 
@@ -11,7 +13,9 @@ const router = express.Router()
 // home / sign in
 router.get('/', (req, res) => {
     try {
-        return res.render('auth/home.ejs')
+        return res.render('auth/home.ejs', {
+            errorMessage: ''
+        })
     } catch (error) {
         console.log(error)
     }
@@ -21,13 +25,77 @@ router.get('/', (req, res) => {
 // sign up 
 router.get('/auth/sign-up', (req, res) => {
     try {
-        return res.render('auth/sign-up.ejs')
+        return res.render('auth/sign-up.ejs', {
+            errorMessage: ''
+        })
     } catch (error) {
         console.log(error)
     }
-}) 
+})
 
 
+// * Create a user *
+router.post('/auth/sign-up', async (req, res) => {
+    try {
+        if (req.body.password !== req.body.passwordConfirmation) {
+            return res.status(422).render('auth/sign-up.ejs', {
+                errorMessage: 'Please make sure your passwords match'
+            })
+        }
+        req.body.password = bcrypt.hashSync(req.body.password, 12)
+
+        const newUser = await User.create(req.body)
+
+        return res.redirect('/')
+
+    } catch (error) {
+        console.log(error.message)
+
+        if(error.code === 11000) {
+            const fieldName = Object.keys(error.keyValue)[0]
+
+            return res.status(422).render('auth/sign-up.ejs', {
+                errorMessage: `That ${fieldName} already exists`
+            })
+        }
+        return res.status(400).render('auth/sign-up.ejs', {
+            errorMessage: error.message
+        })
+    }
+})
+
+
+// * Sign in user *
+router.post('/', async (req, res) => {
+    try {
+        const getUser = await User.findOne({ email: req.body.email })
+
+        if (!getUser) {
+            return res.status(401).render('auth/home.ejs', {
+                errorMessage: 'Unauthorized'
+            })
+        }
+        if (!bcrypt.compareSync(req.body.password, getUser.password)) {
+            return res.status(401).render('auth/home.ejs', {
+                errorMessage: 'Unauthorized'
+            })
+        }
+        req.session.user = {
+            username: getUser.username,
+            email: getUser.email,
+            _id: getUser._id
+        }
+        req.session.save(() => {
+            return res.redirect('/games')
+        })
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+
+
+// * Sign out user *
 
 
 
--- middleware/parser.js
+++ middleware/parser.js
@@ -2,3 +2,26 @@ import { v2 as cloudinary } from 'cloudinary'
 import { CloudinaryStorage } from "multer-storage-cloudinary"
 import multer from 'multer'
 
+// Configure cloudinary
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDNINARY_API_KEY,
+    api_secret: process.env.CLOUDNINARY_API_SECRET
+})
+
+// Storage object - How and where our files are stored
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: 'lagging_legends_uploads',
+        format: 'png',
+        public_id: (req, file) => {
+            return Date.now() + '-' + file.originalname
+        },
+    },
+})
+
+// Parser function - takes the image and provides a req.file key
+const parser = multer({ storage: storage})
+
+export default parser
